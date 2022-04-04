88576ed6f3fd5bcd2ba7a9ecff0e45554856ba37
gnb2000 <barirodriguez@gmail.com>
2022-04-04 11:22:38 -0300

    Initial commit

---
+++ src/main/java/com/recetasAPD/recetasAPD/RecetasApdApplication.java
@@ -0,0 +1,13 @@
+package com.recetasAPD.recetasAPD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetasApdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetasApdApplication.class, args);
+	}
+
+}
